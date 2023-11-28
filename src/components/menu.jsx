8745ab4b013ce32ae1a73ebfda9095da48d33af6
HyperP2502 <59908026+Charlie2502@users.noreply.github.com>
2023-11-28 15:16:09 -0600

    Update menu.jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -75,7 +75,13 @@ export const Menu = () => {
       /* About Us */
 
       <div id='about'>
-
+        <div class="card border-light mb-3" style="max-width: 20rem;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h4 class="card-title">Light card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
       </div>
 
       <MDBFooter className='text-center text-white' style={{ backgroundColor: '#21081a' }}>
