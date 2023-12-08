79f5988056880f69de0a4b4fcb3450a463ccd004
HyperP2502 <59908026+Charlie2502@users.noreply.github.com>
2023-12-08 13:18:01 -0600

    Lib Updt

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,63 +1,45 @@
-import React, { Component } from 'react'
-import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
-import Swal from 'sweetalert2';
+import React from 'react'
 import { useState, useEffect } from 'react';
+import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
 import { auth } from '../config/firebase-config';
-
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+
+import Swal from 'sweetalert2';
+import Popup from 'reactjs-popup';
+
+
+
 
 export const Login = () => {
 
-    const [loginEmail, setLoginEmail] = useState('');
-    const [loginPass, setLoginPass] = useState('');
-    
+    /* HOOKS */
+    const [ISCorreo, setISCorreo] = useState('');
+    const [ISPass, setISPass] = useState('');
+
     const [user, setUser] = useState({});
 
-
     /* Metodos */
-    const registro = async () => {
+    const IS = async () => {
         try {
-            const user = await createUserWithEmailAndPassword(auth, registroEmail, registroPass);
-            Swal.fire({
-                title: "Usuario Creado!",
-                icon: "success"
-              });
-        } catch (error) {
-            Swal.fire({
-                title: "Algo salio mal",
-                text: error.message,
-                icon: "error"
-              });
-        }
-    }
-
-    const login = async () => {
-        try {
-            setUser(await signInWithEmailAndPassword(auth, loginEmail, loginPass)); 
+            /* ISCORREO = INICIAR SESION CORREO | ISCON = INICIAR SESION CONTRASEÑA */
+            setUser(await signInWithEmailAndPassword(auth, ISCorreo, ISPass)); 
             Swal.fire({
                 title: "Sesion Iniciada!",
                 icon: "success"
-              });
+            });
         } catch (error) {
             Swal.fire({
                 title: "Algo salio mal",
                 text: error.message,
                 icon: "error"
-              });
+            });
         }
     }
 
-    const logout = async () => {
-        await signOut(auth);
-        Swal.fire({
-            title: "Sesion Cerrada!",
-            icon: "success"
-          });
-    }
-
+    /* Verificar el incio de sesion*/
     useEffect(() => {
-        onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
+        onAuthStateChanged(auth, (UsuarioActivo) => {
+            setUser(UsuarioActivo);
         });
     }, [user])
 
@@ -73,14 +55,62 @@ export const Login = () => {
                     <fieldset>
                         <div class="form-group">
                             <label class="form-label mt-4">Correo Electronico</label>
-                            <input type="email" class="form-control" value={loginEmail} placeholder="Ingresar correo" onChange={ ({target}) => setLoginEmail(target.value)}/>
+                            <input type="email" class="form-control" value={ISCorreo} placeholder="Ingresar correo" onChange={ ({target}) => setISCorreo(target.value)}/>
                             <label class="form-label mt-4">Contraseña</label>
-                            <input type="email" class="form-control" value={loginPass} placeholder="Ingresar contraseña" onChange={ ({target}) => setLoginPass(target.value)}/>
-                            <button type="button" class="btn btn-dark" onClick={login}>Log In</button>
+                            <input type="email" class="form-control" value={ISPass} placeholder="Ingresar contraseña" onChange={ ({target}) => setISPass(target.value)}/>
+                            <button type="button" class="btn btn-dark" onClick={IS}>Log In</button>
                         </div>
                     </fieldset>
                 </form>
-                <p class="lead">No tiene una cuenta con nosotros? Cree una <a href="">aqui!</a></p>
+                <p class="lead">No tiene una cuenta con nosotros? 
+                    <Popup
+                        trigger={<button className="button"> Open Modal </button>}
+                        modal
+                        nested
+                    >
+                        {close => (
+                        <div className="modal">
+                            <button className="close" onClick={close}>
+                            &times;
+                            </button>
+                            <div className="header"> Modal Title </div>
+                            <div className="content">
+                            {' '}
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+                            Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+                            delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+                            <br />
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+                            commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+                            explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+                            </div>
+                            <div className="actions">
+                            <Popup
+                                trigger={<button className="button"> Trigger </button>}
+                                position="top center"
+                                nested
+                            >
+                                <span>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                                magni omnis delectus nemo, maxime molestiae dolorem numquam
+                                mollitia, voluptate ea, accusamus excepturi deleniti ratione
+                                sapiente! Laudantium, aperiam doloribus. Odit, aut.
+                                </span>
+                            </Popup>
+                            <button
+                                className="button"
+                                onClick={() => {
+                                console.log('modal closed ');
+                                close();
+                                }}
+                            >
+                                close modal
+                            </button>
+                            </div>
+                        </div>
+                        )}
+                    </Popup>
+                </p>
             </div>
 
             <MDBFooter className='text-center text-white' style={{ backgroundColor: '#21081a' }}>
