06b391339e316feaef73da6a700d152841f42517
HyperP2502 <charly.sell@outlook.com>
2023-12-22 20:32:11 -0600

    Update users_manage.jsx

--- src/components/admin/users_manage.jsx
+++ src/components/admin/users_manage.jsx
@@ -1,93 +1,154 @@
-import React, { useEffect, useState } from 'react'
-import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
-import { db } from '../../config/firebase-config';
+import React, { useEffect, useState } from "react";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase-config";
+import btnCellRenderer from "./btnCellRenderer";
+import { AgGridReact } from "ag-grid-react";
 
 export const Users_Manage = () => {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "Users");
 
-    const usersCollectionRef = collection(db, "Users");
+  /* Show Users */
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-    /* Show Users */
-    useEffect(() => {
+    getUsers();
+  }, []);
 
-        const getUsers = async () => {
-            const data = await getDocs(usersCollectionRef);
-            setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        }
+  /* Modify Users */
 
-        getUsers();
-    }, [])
+  /* Delete Users */
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "Users", id);
+    await deleteDoc(userDoc);
+  };
 
-    /* Modify Users */
-
-    /* Delete Users */
-    const deleteUser = async (id) => {
-        const userDoc = doc(db, "Users", id);
-        await deleteDoc(userDoc);
-    }
-
-    return (
-        <>
-            {/* NAVBAR */}
-            <div style={{ paddingBottom: '60px' }}>
-                <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-                    <div className="container-fluid" style={{ padding: 10 }}>
-                        <a className="navbar-brand" style={{ paddingLeft: 20 }} href="#">Larry</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarColor02">
-                            <ul className="navbar-nav me-auto">
-                                <li className="nav-item" style={{ paddingLeft: 20 }}>
-                                    <a className="nav-link" href="/admin/stores" role="button" aria-haspopup="true" aria-expanded="false">Tiendas</a>
-                                </li>
-                                <li className="nav-item" style={{ paddingLeft: 20 }}>
-                                    <a className="nav-link" href="/admin/billing_admin" role="button" aria-haspopup="true" aria-expanded="false">Transacciones</a>
-                                </li>
-                                <li className="nav-item" style={{ paddingLeft: 20 }}>
-                                    <a className="nav-link" href="/admin/users_manage">Usuarios</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+  return (
+    <>
+      {/* NAVBAR */}
+      <div style={{ paddingBottom: "60px" }}>
+        <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+          <div className="container-fluid" style={{ padding: 10 }}>
+            <a className="navbar-brand" style={{ paddingLeft: 20 }} href="#">
+              Larry
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarColor02"
+              aria-controls="navbarColor02"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarColor02">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item" style={{ paddingLeft: 20 }}>
+                  <a
+                    className="nav-link"
+                    href="/admin/stores"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Tiendas
+                  </a>
+                </li>
+                <li className="nav-item" style={{ paddingLeft: 20 }}>
+                  <a
+                    className="nav-link"
+                    href="/admin/billing_admin"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Transacciones
+                  </a>
+                </li>
+                <li className="nav-item" style={{ paddingLeft: 20 }}>
+                  <a className="nav-link" href="/admin/users_manage">
+                    Usuarios
+                  </a>
+                </li>
+              </ul>
             </div>
+          </div>
+        </nav>
+      </div>
 
-            <div class="input-group mb-3" style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
-                <input type="text" class="form-control" placeholder="ID - Nombre - Rol" aria-describedby="button-addon2" />
-                <button class="btn btn-primary" type="button" id="button-addon2">Buscar</button>
+      <div
+        class="input-group mb-3"
+        style={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}
+      >
+        <button
+          type="button"
+          class="btn btn-outline-secondary"
+          style={{ marginLeft: "20px" }}
+        >
+          Añadir Usuario
+        </button>
+      </div>
 
-                <button type="button" class="btn btn-outline-secondary" style={{ marginLeft: '20px' }}>Añadir Usuario</button>
+      <div>
+        {users.map((user) => {
+          const gridOptions = {
+            rowData: [
+              {
+                ID: user.id,
+                Nombre: user.name,
+                Email: user.email,
+                Telefono: user.cellphone,
+              },
+            ],
+            colmunDefs: [
+              { field: "ID", type: "editable" },
+              { field: "Nombre", type: "editable" },
+              { field: "Email", type: "editable" },
+              { field: "Telefono", type: "editableNum" },
+              {
+                field: "acciones",
+                cellRenderer: btnCellRenderer,
+                cellRendererParams: {
+                  clicked: function () {
+                    const deleteUserDocRef = doc(db, "Users", user.id);
+                    deleteDoc(deleteUserDocRef);
+                  },
+                },
+              },
+            ],
+            columnTypes: {
+              editable: {
+                editable: true,
+              },
+              editableNum: {
+                editable: true,
+                valueParser: "Number(newValue)",
+                filter: "agNumberColumnFilter",
+              },
+            },
+          };
+
+          return (
+            <div
+              className={"ag-theme-quartz-dark"}
+              style={{ width: "75%", height: "350px", margin: "auto"}}
+            >
+              <AgGridReact
+                rowData={gridOptions.rowData}
+                columnDefs={gridOptions.colmunDefs}
+              />
             </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-
-            <div>
-                {users.map((user) => {
-                    return (
-                        <table class="table table-hover" style={{ width: '75%', marginLeft: 'auto', marginRight: 'auto' }}>
-                            <tbody>
-                                <tr class="table-active">
-                                    <th scope="row" style={{ width: '5%', textAlign: 'center' }}>{user.id}</th>
-                                    <td style={{ textAlign: 'center' }}>{user.name}</td>
-                                    <td style={{ textAlign: 'center' }}>{user.cellphone}</td>
-                                    <td style={{ textAlign: 'center' }}>{user.email}</td>
-                                    <td style={{ textAlign: 'center' }}>{user.rol}</td>
-                                    <td style={{ textAlign: 'center' }}>
-                                        <button type="button" class="btn btn-warning">Modificar</button>
-                                    </td>
-                                    <td style={{ textAlign: 'center' }}>
-                                        <button type="button" class="btn btn-danger" onClick={() => { deleteUser(user.id) }}>Eliminar</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    );
-                })}
-            </div>
-
-        </>
-    )
-}
-
-export default Users_Manage
+export default Users_Manage;
